3fb60f98ebd7a62cd5480e844ff842fdb630afff
satvik <satvik7773@gmail.com>
2024-01-17 02:41:29 +0530

    prototype 1 implemented front end app

---
+++ app/src/main/java/com/example/app/WeatherX.kt
@@ -0,0 +1,8 @@
+package com.example.app
+
+data class WeatherX(
+    val description: String,
+    val icon: String,
+    val id: Int,
+    val main: String
+)
---
+++ app/src/main/java/com/example/app/Wind.kt
@@ -0,0 +1,7 @@
+package com.example.app
+
+data class Wind(
+    val deg: Int,
+    val gust: Double,
+    val speed: Double
+)
