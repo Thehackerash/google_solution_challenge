f8c9768ee9b669d7f1fafd42c859146a0017c84e
Jarviss77 <gupta.shubham2404@gmail.com>
2024-01-30 01:14:52 +0530

    Revert "Routes for other cities added"
    This reverts commit 246687c4257234bb1a79c4a79a78b3663a33d1e7.

--- API/app.py
+++ API/app.py
@@ -1,10 +1,7 @@
 from flask import Flask, jsonify, request
 import pickle
 
-delhi = pickle.load(open('delhi.pkl', 'rb'))
-bangalore = pickle.load(open('bangalore.pkl', 'rb'))
-lucknow = pickle.load(open('lucknow.pkl', 'rb'))
-patna = pickle.load(open('patna.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 app = Flask(__name__)
 
@@ -12,36 +9,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/predict/delhi', methods=['POST'])
-def delhi_predict():
+@app.route('/predict', methods=['POST'])
+def predict():
     start = int(request.args.get('start'))
     end = int(request.args.get('end'))
 
-    pred_aqidata = delhi.predict(start=start,end=end)
-    return jsonify(pred_aqidata.tolist())
-
-@app.route('/predict/bangalore', methods=['POST'])
-def bangalore_predict():
-    start = int(request.args.get('start'))
-    end = int(request.args.get('end'))
-
-    pred_aqidata = bangalore.predict(start=start,end=end)
-    return jsonify(pred_aqidata.tolist())
-
-@app.route('/predict/lucknow', methods=['POST'])
-def lucknow_predict():
-    start = int(request.args.get('start'))
-    end = int(request.args.get('end'))
-
-    pred_aqidata = lucknow.predict(start=start,end=end)
-    return jsonify(pred_aqidata.tolist())
-
-@app.route('/predict/patna', methods=['POST'])
-def patna_predict():
-    start = int(request.args.get('start'))
-    end = int(request.args.get('end'))
-
-    pred_aqidata = patna.predict(start=start,end=end)
+    pred_aqidata = model.predict(start=start,end=end)
     return jsonify(pred_aqidata.tolist())
 
 if __name__ == '__main__':
