12e4b4ced4d5a83765af40b551fbd8facfcf3876
Jarviss77 <gupta.shubham2404@gmail.com>
2024-01-31 20:13:14 +0530

    Modified API

--- API/app.py
+++ API/app.py
@@ -1,11 +1,14 @@
 from logging import Logger
 from flask import Flask, jsonify, request
 import pickle
+import pandas as pd
 
 delhi = pickle.load(open('delhi.pkl', 'rb'))
 bangalore = pickle.load(open('bangalore.pkl', 'rb'))
 lucknow = pickle.load(open('lucknow.pkl', 'rb'))
 patna = pickle.load(open('patna.pkl', 'rb'))
+
+# list = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 app = Flask(__name__)
 
@@ -19,7 +22,17 @@ def delhi_predict():
     end = int(request.args.get('end'))
 
     pred_aqidata = delhi.predict(start=start,end=end)
-    return jsonify(pred_aqidata.tolist())
+    data = pd.Series(pred_aqidata)
+    data_dict = data.to_dict()
+    json_data = {}
+    for key, value in data_dict.items():
+        year = key.year
+        month = key.strftime("%B")
+        if year not in json_data:
+            json_data[year] = {}
+        json_data[year][month] = value
+
+    return jsonify(json_data)
 
 @app.route('/predict/bangalore', methods=['GET'])
 def bangalore_predict():
@@ -27,7 +40,17 @@ def bangalore_predict():
     end = int(request.args.get('end'))
 
     pred_aqidata = bangalore.predict(start=start,end=end)
-    return jsonify(pred_aqidata.tolist())
+    data = pd.Series(pred_aqidata)
+    data_dict = data.to_dict()
+    json_data = {}
+    for key, value in data_dict.items():
+        year = key.year
+        month = key.strftime("%B")
+        if year not in json_data:
+            json_data[year] = {}
+        json_data[year][month] = value
+
+    return jsonify(json_data)
 
 @app.route('/predict/lucknow', methods=['GET'])
 def lucknow_predict():
@@ -35,7 +58,17 @@ def lucknow_predict():
     end = int(request.args.get('end'))
 
     pred_aqidata = lucknow.predict(start=start,end=end)
-    return jsonify(pred_aqidata)
+    data = pd.Series(pred_aqidata)
+    data_dict = data.to_dict()
+    json_data = {}
+    for key, value in data_dict.items():
+        year = key.year
+        month = key.strftime("%B")
+        if year not in json_data:
+            json_data[year] = {}
+        json_data[year][month] = value
+
+    return jsonify(json_data)
 
 @app.route('/predict/patna', methods=['GET'])
 def patna_predict():
@@ -43,7 +76,17 @@ def patna_predict():
     end = int(request.args.get('end'))
 
     pred_aqidata = patna.predict(start=start,end=end)
-    return jsonify(pred_aqidata)
+    data = pd.Series(pred_aqidata)
+    data_dict = data.to_dict()
+    json_data = {}
+    for key, value in data_dict.items():
+        year = key.year
+        month = key.strftime("%B")
+        if year not in json_data:
+            json_data[year] = {}
+        json_data[year][month] = value
+
+    return jsonify(json_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
