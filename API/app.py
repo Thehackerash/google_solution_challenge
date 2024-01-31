c3e26bf6ba7ce59acbf9622b47f1f6c37299f713
Jarviss77 <gupta.shubham2404@gmail.com>
2024-01-31 10:34:22 +0530

    Changed request method

--- API/app.py
+++ API/app.py
@@ -1,3 +1,4 @@
+from logging import Logger
 from flask import Flask, jsonify, request
 import pickle
 
@@ -12,7 +13,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/predict/delhi', methods=['POST'])
+@app.route('/predict/delhi', methods=['GET'])
 def delhi_predict():
     start = int(request.args.get('start'))
     end = int(request.args.get('end'))
@@ -20,7 +21,7 @@ def delhi_predict():
     pred_aqidata = delhi.predict(start=start,end=end)
     return jsonify(pred_aqidata.tolist())
 
-@app.route('/predict/bangalore', methods=['POST'])
+@app.route('/predict/bangalore', methods=['GET'])
 def bangalore_predict():
     start = int(request.args.get('start'))
     end = int(request.args.get('end'))
@@ -28,21 +29,21 @@ def bangalore_predict():
     pred_aqidata = bangalore.predict(start=start,end=end)
     return jsonify(pred_aqidata.tolist())
 
-@app.route('/predict/lucknow', methods=['POST'])
+@app.route('/predict/lucknow', methods=['GET'])
 def lucknow_predict():
     start = int(request.args.get('start'))
     end = int(request.args.get('end'))
 
     pred_aqidata = lucknow.predict(start=start,end=end)
-    return jsonify(pred_aqidata.tolist())
+    return jsonify(pred_aqidata)
 
-@app.route('/predict/patna', methods=['POST'])
+@app.route('/predict/patna', methods=['GET'])
 def patna_predict():
     start = int(request.args.get('start'))
     end = int(request.args.get('end'))
 
     pred_aqidata = patna.predict(start=start,end=end)
-    return jsonify(pred_aqidata.tolist())
+    return jsonify(pred_aqidata)
 
 if __name__ == '__main__':
     app.run(debug=True)
